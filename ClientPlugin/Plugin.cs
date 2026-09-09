97fc66a23531d13453f1d169065d6a751b02562f
Viktor Ferenczi <viktor@ferenczi.eu>
2026-09-10 01:33:43 +0200

    1.0.5

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -13,8 +13,8 @@ using VRage.Utils;
 
 // Define assembly version when compiled by Pulsar
 #if !DEV_BUILD
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 #endif
 
 namespace ClientPlugin;
